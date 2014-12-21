7f5e5e3615e9e20835de2dd0f282ab46e242d5b6
David Mann <mannd@epstudiossoftware.com>
2014-12-20 20:41:50 -0700

    changed font to custom font

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 static Window* s_main_window;
 static TextLayer* s_time_layer;
+static GFont s_time_font;
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -24,18 +25,23 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
 
 static void main_window_load(Window* window) {
-  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  s_time_font = fonts_load_custom_font(resource_get_handle(
+    RESOURCE_ID_RESOURCE_ID_FONT_PERFECT_DOS_48));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+
+  update_time();
 }
 
 static void main_window_unload(Window* window) {
   text_layer_destroy(s_time_layer);
+  fonts_unload_custom_font(s_time_font);
   
 }
 
@@ -47,7 +53,6 @@ static void init() {
 });
   window_stack_push(s_main_window, true);
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  update_time();
 }
 
 static void deinit() {
