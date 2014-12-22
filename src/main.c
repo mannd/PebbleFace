4416decd10c91fdd60c559be318a8b5119901b47
David Mann <mannd@epstudiossoftware.com>
2014-12-22 12:03:38 -0700

    added weather text layer

--- src/main.c
+++ src/main.c
@@ -2,21 +2,61 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_weather_layer;
 static GFont s_time_font;
+static GFont s_weather_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
+
+// function from http://forums.getpebble.com/discussion/comment/29591/
+// as Pebble doesn't have itoa or sprintf,
+// for use in MorbidMeterPebble? Not used here.
+char *itoa(int num)
+{
+	static char buff[20] = {};
+	int i = 0, temp_num = num, length = 0;
+	char *string = buff;
+	
+	if(num >= 0) {
+		// count how many characters in the number
+		while(temp_num) {
+			temp_num /= 10;
+			length++;
+		}
+		
+		// assign the number to the buffer starting at the end of the 
+		// number and going to the begining since we are doing the
+		// integer to character conversion on the last number in the
+		// sequence
+		for(i = 0; i < length; i++) {
+		 	buff[(length-1)-i] = '0' + (num % 10);
+			num /= 10;
+		}
+		buff[i] = '\0'; // can't forget the null byte to properly end our string
+	}
+	else
+		return "Unsupported Number";
+	
+	return string;
+}
+
+
 static void update_time() {
+  // time_t is usual msec value used by MorbidMeter
   time_t temp = time(NULL);
+  // MM would manipulate this value here
   struct tm *tick_time = localtime(&temp);
   
-  static char buffer[] = "00:00";
+  static char *buffer = "00:00";
   
+  // MM would format value properly here
   if (clock_is_24h_style() == true) {
     strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
   } else {
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
+//   buffer = itoa(temp);
   
   text_layer_set_text(s_time_layer, buffer);
 }
@@ -41,9 +81,17 @@ static void main_window_load(Window* window) {
     RESOURCE_ID_RESOURCE_ID_FONT_PERFECT_DOS_48));
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 
+  s_weather_layer = text_layer_create(GRect(0, 130, 144, 25));
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer, GColorWhite);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "Loading...");
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_RESOURCE_ID_FONT_PERFECT_DOS_20));
+  text_layer_set_font(s_weather_layer, s_weather_font);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+  
   update_time();
 }
 
@@ -52,6 +100,8 @@ static void main_window_unload(Window* window) {
   fonts_unload_custom_font(s_time_font);
   gbitmap_destroy(s_background_bitmap);
   bitmap_layer_destroy(s_background_layer);
+  text_layer_destroy(s_weather_layer);
+  fonts_unload_custom_font(s_weather_font);
   
 }
 
